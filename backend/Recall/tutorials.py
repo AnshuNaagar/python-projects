70db73eb41dfe67d3862c7d04a2ee333caaaba0c
Anshu Naagar <83154441+AnshuNaagar@users.noreply.github.com>
2022-05-10 07:19:32 +0530

    Add files via upload

---
+++ backend/Recall/tutorials.py
@@ -0,0 +1,18 @@
+# <------------------------> all sample code <------------------------>
+
+print("Hello world")
+
+fruit_names = ["Banana","apples","boiler"]
+x,y,z = fruit_names
+
+print(x)
+print(x)
+print(y)
+
+
+name = (input("Enter your name : "));
+className = (input("Enter your class : "));
+makeVariable = (input("Make a vairable name"));
+
+for i in range(10):
+    print(i)
